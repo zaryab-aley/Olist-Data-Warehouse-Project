3dbcc89d8957cb290a573ff96fb919f608c06aff
Zaryab Ali <zairy911@gmail.com>
2026-06-16 22:28:16 +0100

    bronze ddl

---
+++ bronze_layer.sql
@@ -0,0 +1,78 @@
+create table bronze.customers_dataset(
+customer_id nvarchar(50),
+customer_unique_id nvarchar(50),
+customer_zip_code_prefix int,
+customer_city nvarchar(50),
+customer_state nvarchar(5)
+)
+
+create table bronze.geolocation_dataset(
+geolocation_zip_code_prefix int,
+geolocation_lat float,
+geolocation_lng float,
+geolocation_city nvarchar(50),
+geolocation_state nvarchar(5)
+)
+
+create table bronze.order_items_dataset(
+order_id nvarchar(50),
+order_item_id nvarchar(50),
+product_id nvarchar(50),
+seller_id nvarchar(50),
+shipping_limit_date datetime2,
+price decimal,
+freight_value decimal
+)
+
+create table bronze.order_payments_dataset(
+order_id nvarchar(50),
+payment_sequential int,
+payment_type nvarchar(50),
+payment_installments int,
+payment_value decimal
+)
+
+create table bronze.order_reviews_dataset(
+review_id nvarchar(50),
+order_id nvarchar(50),
+review_score int,
+review_comment_title nvarchar(200),
+review_comment_message nvarchar(200),
+review_creation_date datetime2,
+review_answer_timestamp datetime2
+)
+
+create table bronze.orders_dataset(
+order_id nvarchar(50),
+customer_id nvarchar(50),
+order_status nvarchar(50),
+order_purchase_timestamp datetime2,
+order_approved_at datetime2,
+order_delivered_carrier_date datetime2,
+order_delivered_customer_date datetime2,
+order_estimated_delivery_date datetime2
+)
+
+create table bronze.products_dataset(
+product_id nvarchar(50),
+product_category_name nvarchar(50),
+product_name_lenght int,
+product_description_lenght int,
+product_photos_qty int,
+product_weight_g int,
+product_length_cm int,
+product_height_cm int,
+product_width_cm int
+)
+
+create table bronze.sellers_dataset(
+seller_id nvarchar(50),
+seller_zip_code_prefix int,
+seller_city nvarchar(50),
+seller_state nvarchar(5)
+)
+
+create table product_category_name_translation(
+product_category_name nvarchar(50),
+product_category_name_english nvarchar(50)
+)
