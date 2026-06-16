02fca924e78c87c58cec122e91a46258d4c14c0e
Zaryab Ali <zairy911@gmail.com>
2026-06-16 21:47:04 +0100

    added

--- database_creation_script.sql
+++ database_creation_script.sql
@@ -1,3 +1,13 @@
+use master;
+go
+
+if exists (select 1 from sys.databases where name='olist_data_warehouse')
+begin
+	alter database olist_data_warehouse set single_user with rollback immediate
+	drop database olist_data_warehouse;
+end;
+go
+
 create database olist_data_warehouse;
 
 use olist_data_warehouse;
